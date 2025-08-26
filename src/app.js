1529006e159499ebfc6079b9cef26ab02cbd2cc4
Mhd-Muzammil <zamil627810@gmail.com>
2025-08-26 20:23:58 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.get("/User/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
+app.use("/User", (req, res, next) => {
+    console.log("server 1!!");
+    // res.send("Response1");
+    next();
+},
+    (req, res, next) => {
+        console.log("server 2!!");
+        // res.send("Response2");
+        next();
     
-    res.send({ Fname: "mohamed", Lname: "Mustaqeem" });
+},
+    (req, res, next) => {
+        console.log("server 3!!");
+        res.send("Response3");
+        next();
+    
 });
-
 
 
 
